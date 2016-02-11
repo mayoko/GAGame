15c4c9411425860f36f3a7098776f90a4757501d
cexen <cexenial@gmail.com>
2016-02-12 02:23:33 +0900

    [add]自機の等速回転
    自機が（ひとまず）1秒におよそ1周のスピードで回るようにした．
    
    frameの長さによらず1frameあたり一定角速度で回るようにしている．
    環境/倍速によらず結果の再現性を確保するため．

---
+++ GAGame/Assets/Scripts/SakeruCheesePlayerController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SakeruCheesePlayerController : MonoBehaviour {
+    // angleVelにはstaticをつけないとありえないほど回転が遅くなる（多分最適化が効かないせい）
+    // cexen環境ではdeltaTimeは約 0.0165 s/frame
+    public static float angleVel = 360.0f * 0.0165f; // 角速度[度/frame]
+    void Update() {
+        // 自身をy-軸周りに回転
+        // 回転角[度/frame]は1フレームの間隔[s/frame]によらず一定（再現性が重要ゆえ）
+        transform.Rotate(0, angleVel, 0);
+    }
+}
