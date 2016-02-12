ffc887f549e81cd85e5893c47080f7c1d51f2c2b
cexen <cexenial@gmail.com>
2016-02-12 18:07:54 +0900

    [add]SC当たり判定
    SakeruCheeseのPlayerとEnemyの当たり判定を実装した．
    接触するとGeneCalcに飛ぶ．

---
+++ GAGame/Assets/Scripts/SCPlayerSphereController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SCPlayerSphereController : MonoBehaviour {
+    void OnTriggerEnter (Collider hit)
+    {
+        if (hit.CompareTag ("Enemy"))
+        {
+            Debug.Log("unko");
+            SceneManager.LoadScene("GeneCalc");
+        }
+    }
+}
