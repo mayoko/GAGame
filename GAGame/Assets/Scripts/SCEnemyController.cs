5e6f5c82d3888aafc98bac7b6bcfab6b6e048282
hydromole <meip2015ykfqmlykflqik@gmail.com>
2016-02-17 11:36:43 +0900

    [add]Enemyのスピードもデータから読み込めるようにした

--- GAGame/Assets/Scripts/SCEnemyController.cs
+++ GAGame/Assets/Scripts/SCEnemyController.cs
@@ -2,7 +2,13 @@
 using System.Collections;
 
 public class SCEnemyController : MonoBehaviour {
-	public static float enemySpeed = 1;
+	private float enemySpeed; //publicからprivateに変更.そうでないと全部の敵のスピードが等しくなる
+	public SCGameController refSCGC;
+
+	void Start(){
+		refSCGC = GameObject.Find ("GameController").GetComponent<SCGameController>();
+		enemySpeed = refSCGC.enemyInfo [refSCGC.enemyNum - 1] [2]; //呼び出すタイミングの問題か-1が必要
+	}
 
 	void Update () {
 		transform.Translate (-1 * 0.0165f * enemySpeed, 0, 0);
