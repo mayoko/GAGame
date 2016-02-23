155d7d971db7c92c5eee9c732cd229a83a1abe5d
mayoko <mayokoex@gmail.com>
2016-02-23 16:44:37 +0900

    [add] frameCount 用のスライダーを追加

--- GAGame/Assets/Scripts/gotogame.cs
+++ GAGame/Assets/Scripts/gotogame.cs
@@ -5,11 +5,13 @@ using UnityEngine.SceneManagement;
 public class gotogame : MonoBehaviour {
     public UnityEngine.UI.Slider playerNumBar;
     public UnityEngine.UI.Slider mutationRateBar;
+    public UnityEngine.UI.Slider frameCountBar;
     // シーンの移動(引数と入して渡されたシーンを呼ぶ)
     public void toMainScene()
 	{
         GeneManager.param.mutationRate = mutationRateBar.value;
         GeneManager.param.playerNum = (int)(playerNumBar.value);
+        GeneManager.param.playFrame = (int)(frameCountBar.value);
         GeneManager.init();
         SceneManager.LoadScene("SakeruCheese");
     }
