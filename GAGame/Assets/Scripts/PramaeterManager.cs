3138e8cf0f988e3c4f16a926949e38aff84e0d72
okahukurou <okadamako113@yahoo.co.jp>
2016-04-13 01:08:57 +0900

    [add]UIの新たな仕組みテスト
    あくまで実験用で全く整えていません。とりあえず、項目名の上で説明が表示される機能と、数字を押すと設定できるようになる機能を入れてみたので、使用感を聞いてみようと思います。

---
+++ GAGame/Assets/Scripts/PramaeterManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PramaeterManager : MonoBehaviour {
+	public UnityEngine.UI.Text SelectedText;
+	public UnityEngine.UI.Text SelectedValue;
+	public UnityEngine.UI.Slider SelectedSlider;
+
+	public UnityEngine.UI.Text GroupSizeValue;
+
+	string selected;
+
+	// Use this for initialization
+	void Start () {
+		GroupSizeValue.text = GeneManager.param.playerNum.ToString ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ChangeSelected(string name)
+	{
+		if (name == selected) {
+			return;
+		}
+		switch (name) 
+		{
+		case "GroupSize":
+			SelectedText.text = "GroupSize";
+			selected = "Setting";
+			SelectedValue.text = GeneManager.param.playerNum.ToString ();
+			SelectedSlider.interactable = true;
+			SelectedSlider.value = (float)GeneManager.param.playerNum;
+			SelectedSlider.minValue = (float)0;
+			SelectedSlider.maxValue = (float)100;
+			selected = "GroupSize";
+			break;
+		}
+	}
+	public void SliderValueChange()
+	{
+		switch (selected)
+		{
+		case "GroupSize":
+			GeneManager.param.playerNum = (int)SelectedSlider.value;
+			SelectedValue.text = GeneManager.param.playerNum.ToString();
+			GroupSizeValue.text = GeneManager.param.playerNum.ToString();
+			break;
+		}
+	}
+}
