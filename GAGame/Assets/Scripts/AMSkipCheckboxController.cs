29924b714e7b914ff466b8b8202a7ff7c4741696
cexen <cexenial@gmail.com>
2016-04-20 09:38:04 +0900

    [add]GC: SkipトグルとSpeedスライダーのUI・処理を追加

---
+++ GAGame/Assets/Scripts/AMSkipCheckboxController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AMSkipCheckboxController : MonoBehaviour {
+    GeneManager.ViewParam vp = GeneManager.viewParam;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Toggle>().onValueChanged.AddListener(OnValueChanged); // 登録しないと呼ばれない
+        GetComponent<Toggle>().isOn = vp.isSkipping;
+    }
+
+    void OnValueChanged(bool toggle) {
+        vp.isSkipping = toggle;
+	}
+}
---
+++ GAGame/Assets/Scripts/AMSpeedSlider.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+
+public class AMSpeedSlider : MonoBehaviour {
+    GeneManager.ViewParam vp = GeneManager.viewParam;
+
+	void Start ()
+    {
+        GetComponent<Slider>().onValueChanged.AddListener(OnValueChanged);
+        GetComponent<Slider>().value = SpeedToValue(vp.playSpeed);
+	}
+
+    void OnValueChanged (float value)
+    {
+        vp.playSpeed = ValueToSpeed(value);
+        transform.FindChild("Text").GetComponent<Text>().text = "x" + vp.playSpeed;
+    }
+
+    float ValueToSpeed (float value)
+    {
+        return (float)Math.Pow(2, value);
+    }
+    float SpeedToValue (float speed)
+    {
+        return (float)Math.Log(speed, 2);
+    }
+}
