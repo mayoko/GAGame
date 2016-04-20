4a8975d046836a136303d9c8bcb37c6aa4a18ce2
cexen <cexenial@gmail.com>
2016-04-20 09:53:29 +0900

    [add]AM: Skipトグルに実際にスキップする機能をつけた

--- GAGame/Assets/Scripts/AMSkipCheckboxController.cs
+++ GAGame/Assets/Scripts/AMSkipCheckboxController.cs
@@ -3,15 +3,18 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class AMSkipCheckboxController : MonoBehaviour {
+    public AMTestAnimation ta;
     GeneManager.ViewParam vp = GeneManager.viewParam;
 
 	// Use this for initialization
 	void Start () {
-        GetComponent<Toggle>().onValueChanged.AddListener(OnValueChanged); // 登録しないと呼ばれない
         GetComponent<Toggle>().isOn = vp.isSkipping;
+        if (vp.isSkipping) ta.Skip(); // スキップモードなら即スキップ
+        GetComponent<Toggle>().onValueChanged.AddListener(OnValueChanged); // 登録しないと呼ばれない
     }
 
     void OnValueChanged(bool toggle) {
         vp.isSkipping = toggle;
+        if (toggle) ta.Skip();
 	}
 }
