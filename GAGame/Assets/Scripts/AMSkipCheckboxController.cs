f79adc2b9ce17db71a84c421a2b9a86c287cbfa6
cexen <cexenial@gmail.com>
2016-04-20 10:23:33 +0900

    [fix] Skip機能をGeneCalcでのみ働くように修正

--- GAGame/Assets/Scripts/AMSkipCheckboxController.cs
+++ GAGame/Assets/Scripts/AMSkipCheckboxController.cs
@@ -9,12 +9,12 @@ public class AMSkipCheckboxController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         GetComponent<Toggle>().isOn = vp.isSkipping;
-        if (vp.isSkipping) ta.Skip(); // スキップモードなら即スキップ
+        if (ta && vp.isSkipping) ta.Skip(); // スキップモードなら即スキップ
         GetComponent<Toggle>().onValueChanged.AddListener(OnValueChanged); // 登録しないと呼ばれない
     }
 
     void OnValueChanged(bool toggle) {
         vp.isSkipping = toggle;
-        if (toggle) ta.Skip();
+        if (ta && toggle) ta.Skip(); // SakeruCheeseではtaが無い
 	}
 }
