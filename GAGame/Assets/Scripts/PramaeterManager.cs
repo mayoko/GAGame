134df9e2140d4d5cd28718a873afef2c5ceb227f
okahukurou <okadamako113@yahoo.co.jp>
2016-05-04 13:21:05 +0900

    [Modify]タイトル画面のUI
    クリックしたら説明を表示するように変更した。

--- GAGame/Assets/Scripts/PramaeterManager.cs
+++ GAGame/Assets/Scripts/PramaeterManager.cs
@@ -2,52 +2,25 @@
 using System.Collections;
 
 public class PramaeterManager : MonoBehaviour {
-	public UnityEngine.UI.Text SelectedText;
-	public UnityEngine.UI.Text SelectedValue;
-	public UnityEngine.UI.Slider SelectedSlider;
-
 	public UnityEngine.UI.Text GroupSizeValue;
-
-	string selected;
+	public UnityEngine.UI.Slider GroupSizeSlider;
 
 	// Use this for initialization
 	void Start () {
 		GroupSizeValue.text = GeneManager.param.playerNum.ToString ();
+		GroupSizeSlider.value = GeneManager.param.playerNum;
+		GroupSizeSlider.maxValue = 100;
+		GroupSizeSlider.minValue = 20;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
-
-	public void ChangeSelected(string name)
+		
+	public void changeGroupSize()
 	{
-		if (name == selected) {
-			return;
-		}
-		switch (name) 
-		{
-		case "GroupSize":
-			SelectedText.text = "GroupSize";
-			selected = "Setting";
-			SelectedValue.text = GeneManager.param.playerNum.ToString ();
-			SelectedSlider.interactable = true;
-			SelectedSlider.value = (float)GeneManager.param.playerNum;
-			SelectedSlider.minValue = (float)0;
-			SelectedSlider.maxValue = (float)100;
-			selected = "GroupSize";
-			break;
-		}
-	}
-	public void SliderValueChange()
-	{
-		switch (selected)
-		{
-		case "GroupSize":
-			GeneManager.param.playerNum = (int)SelectedSlider.value;
-			SelectedValue.text = GeneManager.param.playerNum.ToString();
-			GroupSizeValue.text = GeneManager.param.playerNum.ToString();
-			break;
-		}
+		GeneManager.param.playerNum=(int)GroupSizeSlider.value;
+		GroupSizeValue.text = GeneManager.param.playerNum.ToString ();
 	}
 }
